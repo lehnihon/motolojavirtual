5efcef7fa5887e8a5d61d78247b9da01ea38ed00
Leandro <lehnihon@hotmail.com>
2016-06-03 17:04:59 -0300

    descriçoes footer

--- footer.php
+++ footer.php
@@ -3,16 +3,19 @@
 			<div class="container">
 				<div class="row main">
 					<div class="col-md-6 produtos clearfix">
-						<strong>Produtos Space Cell</strong><br><br>
+						<strong>Produtos Mega Moto Peças</strong><br><br>
 						<ul>
-							<li><a href="<?php echo esc_url( home_url( '/' )."categoria/meu" ); ?>">Meu</a></li>
-							<li><a href="<?php echo esc_url( home_url( '/' )."categoria/celulares" ); ?>">Celulares</a></li>
-							<li><a href="<?php echo esc_url( home_url( '/' )."categoria/mallory" ); ?>">Mallory</a></li>
+							<li><a href="<?php echo home_url( '/' )."categoria/acessorios"; ?>">Acessórios</a></li>
+							<li><a href="<?php echo home_url( '/' )."categoria/motos-nacionais"; ?>">Motos Nacionais</a></li>
+							<li><a href="<?php echo home_url( '/' )."categoria/motos-importadas"; ?>">Motos Importadas</a></li>
+							<li><a href="<?php echo home_url( '/' )."categoria/motor-completo"; ?>">Motor Completo</a></li>
+							<li><a href="<?php echo home_url( '/' )."categoria/koso"; ?>">Koso</a></li>
+							<li><a href="<?php echo home_url( '/' )."categoria/juntas-para-motores"; ?>">Juntas para Motores</a></li>
 						</ul>									
 																
 					</div>
 					<div class="col-md-3">
-						<strong>Sobre a Space Cell</strong><br><br>
+						<strong>Sobre a Mega Moto Peças</strong><br><br>
 						<ul>
 							<li><a href="<?php echo esc_url( home_url( '/' )."sobre" ); ?>">Institucional</a></li>
 							<li><a href="<?php echo esc_url( home_url( '/' )."politica-de-privacidade" ); ?>">Política de Privacidade</a></li>
@@ -49,7 +52,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-10">
-						&copy; Copyright <?php echo date('Y') ?> - SpaceCell | <a target="_blank" href="http://agenciarino.com.br/">Site desenvolvido pela Agência Rino.</a>
+						&copy; Copyright <?php echo date('Y') ?> - Mega Moto Peças | <a target="_blank" href="http://agenciarino.com.br/">Site desenvolvido pela Agência Rino.</a>
 					</div>
 					<div class="col-md-2">
 						<a target="_blank" href="http://agenciarino.com.br/"><img class="anima-rino" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/footer-rino.png"?>" /></a>	
